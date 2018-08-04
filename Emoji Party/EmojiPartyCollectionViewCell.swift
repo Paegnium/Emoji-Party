0d4579048c131981b1e6084ff72063b2d3677aef
Wylie <paegnium@gmail.com>
2018-08-04 09:47:56 +0800

    Basic stuff laid out

---
+++ Emoji Party/EmojiPartyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiPartyCollectionViewCell.swift
+//  Emoji Party
+//
+//  Created by Wylie Quah on 4/8/18.
+//  Copyright © 2018 Edgefield. All rights reserved.
+//
+
+import UIKit
+
+class EmojiPartyCollectionViewCell: UICollectionViewCell {
+    
+}
